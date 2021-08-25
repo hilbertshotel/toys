5928cc91e084674f6877c007f576d1cce3c7a3a5
Nikolay Kalchev <nikolay.kalchev@abv.bg>
2021-08-25 22:13:06 +0300

    added colored tiles

--- games/grabatile/scripts/lib.js
+++ games/grabatile/scripts/lib.js
@@ -1,3 +1,5 @@
+"use strict"
+
 // GET
 let get = (id) => {
     return document.getElementById(id)
@@ -31,4 +33,17 @@ let make = (type, ...properties) => {
 // SLEEP
 let sleep = (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+// SHUFFLE
+let shuffle = (array) => {
+    for (let i=array.length-1; i>0; i--) {
+        let rand = Math.floor(Math.random() * (i+1));
+        [array[i], array[rand]] = [array[rand], array[i]]
+    }
+}
+
+// RANDOM
+let random = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
