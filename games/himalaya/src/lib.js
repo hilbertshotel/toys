c29c3ee7a1f609ceeb9fa691686df3a3f02bd131
Nikolay Kalchev <nikolay.kalchev@abv.bg>
2021-09-22 01:22:13 +0300

    need to fix number design

--- games/himalaya/src/lib.js
+++ games/himalaya/src/lib.js
@@ -1,56 +1,61 @@
 // GET
 const get = (id) => {
-    return document.getElementById(id)
+  return document.getElementById(id)
 }
 
 // CLASS
 const getClass = (name, pos=-1) => {
-    const c = document.getElementsByClassName(name)
-    if (pos < 0) {
-        return c
-    } else {
-        return c[pos]
-    }
+  const c = document.getElementsByClassName(name)
+  if (pos < 0) {
+      return c
+  } else {
+      return c[pos]
+  }
 }
 
 // MAKE
 const MAKEOBJECT = {
-    class: (e, v) => { e.className = v },
-    id: (e, v) => { e.id = v },
-    text: (e, v) => { e.innerHtml = v }
+  class: (e, v) => { e.className = v },
+  id: (e, v) => { e.id = v },
+  text: (e, v) => { e.innerHTML = v }
 }
 
 const make = (type, ...properties) => {
-    const element = document.createElement(type)
-    for (const property of properties) {
-        const [p1, p2] = property.split("=")
-        MAKEOBJECT[p1](element, p2)
-    }
-    return element
+  const element = document.createElement(type)
+  for (const property of properties) {
+    const [p1, p2] = property.split("=")
+    MAKEOBJECT[p1](element, p2)
+  }
+  return element
 }
 
 // SLEEP
 const sleep = (ms) => {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 // RAND
 const rand = (min, max) => {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1) + min)
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 // INSERT
 const insert = (parent, ...elements) => {
-    for (const element of elements) {
-        parent.append(element)
-    }
+  for (const element of elements) {
+    parent.append(element)
+  }
 }
 
 // INSERT AFTER BEGIN
 const insertAtStart = (parent, ...elements) => {
-    for (const element of elements) {
-        parent.insertAdjacentElement("afterbegin", element)
-    }
+  for (const element of elements) {
+    parent.insertAdjacentElement("afterbegin", element)
+  }
+}
+
+// CLEAR
+const clear = (element) => {
+  element.innerHTML = ""
 }
--- games/himalaya/src/main.js
+++ games/himalaya/src/main.js
@@ -1,123 +1,165 @@
+// GLOBALS
+// ================================================================================
+
+let NEXT_NUMBER = 0
+
+
 // LOGIC
 // ================================================================================
 
 const animationName = (num) => {
-    if (num < 20) {
-        return "clouds-left"
-    } else if (num > 60) {
-        return "clouds-right"
-    } else {
-        return ["clouds-left", "clouds-right"][rand(0, 1)]
-    }
+  if (num < 20) {
+    return "clouds-left"
+  } else if (num > 60) {
+    return "clouds-right"
+  } else {
+    return ["clouds-left", "clouds-right"][rand(0, 1)]
+  }
 }
 
 
 const position = (num) => {
-    if (num < 30) {
-        return 0
-    } else {
-        return 1
-    }
+  if (num < 30) {
+    return 0
+  } else {
+    return 1
+  }
 }
 
 
 const createClouds = (n) => {
-    let clouds = []
+  let clouds = []
 
-    for (let i=0; i<n; i++) {
-        let
-            randTop = rand(5, 70),
-            randLeft = rand(0, 70)
-        
-        let cloud = {
-            top: `${randTop}%`,
-            left: `${randLeft}%`,
-            nameAnim: animationName(randLeft),
-            durAnim: `${rand(300, 400)}s`,
-            pos: position(randTop),
-        }
-
-        clouds.push(cloud)
+  for (let i=0; i<n; i++) {
+    const
+      randTop = rand(5, 70),
+      randLeft = rand(0, 70)
+      
+    const cloud = {
+      top: `${randTop}%`,
+      left: `${randLeft}%`,
+      nameAnim: animationName(randLeft),
+      durAnim: `${rand(300, 400)}s`,
+      pos: position(randTop),
     }
 
-    return clouds
+    clouds.push(cloud)
+  }
+
+  return clouds
 }
 
 
 // DOM
 // ================================================================================
 
-const newCloudDiv = () => {
-    let
-        cloudDiv = make("div", "class=cloud"),
-        cleft = make("div", "class=cloudling", "id=cleft"),
-        cbottom = make("div", "class=cloudling", "id=cbottom"),
-        cright = make("div", "class=cloudling", "id=cright"),
-        ctop = make("div", "class=cloudling", "id=ctop")
+const newCloudDiv = (cloudNumber) => {
+  const
+    cloudDiv = make("div", "class=cloud" ,`id=${cloudNumber}`),
+    cleft = make("div", "class=cloudling", "id=cleft"),
+    cbottom = make("div", "class=cloudling", "id=cbottom"),
+    cright = make("div", "class=cloudling", "id=cright"),
+    ctop = make("div", "class=cloudling", "id=ctop")
+    number = make("div", "class=number", `id=n${cloudNumber}`, `text=${cloudDiv.id}`)
 
-    insert(cloudDiv, cleft, cbottom, cright, ctop)
-    return cloudDiv
+  insert(cloudDiv, number, cleft, cbottom, cright, ctop)
+  return cloudDiv
 }
 
 
-const scatterClouds = async (sky, clouds) => {
-    for (const cloud of clouds) {
+const scatterClouds = (sky, clouds) => {
+  for (const [i, cloud] of clouds.entries()) {
 
-        const cloudDiv = newCloudDiv()
-        cloudDiv.onclick = () => { action(cloudDiv) }
+    const cloudDiv = newCloudDiv(i)
+    cloudDiv.onclick = () => { action(cloudDiv) }
 
-        cloudDiv.style.top = cloud.top
-        cloudDiv.style.left = cloud.left
-        cloudDiv.style.animationName = cloud.nameAnim
-        cloudDiv.style.animationDuration = cloud.durAnim
+    cloudDiv.style.top = cloud.top
+    cloudDiv.style.left = cloud.left
+    cloudDiv.style.animationName = cloud.nameAnim
+    cloudDiv.style.animationDuration = cloud.durAnim
 
-        if (cloud.pos === 0) {
-            cloudDiv.style.width = "15%"
-            cloudDiv.style.height = "10%"
-            insertAtStart(sky, cloudDiv)
-        } else {
-            insert(sky, cloudDiv)
-        }
+    if (cloud.pos === 0) {
+      cloudDiv.style.width = "15%"
+      cloudDiv.style.height = "10%"
+      insertAtStart(sky, cloudDiv)
 
-        await sleep(100)
+      // handle smaller font size in back clouds
+      const currentNumber = get(`n${i}`)
+      currentNumber.style.fontSize = "4.3vh"
+      currentNumber.style.top = "-35%"
+      currentNumber.style.left = "35%"
     }
+    
+    else {
+      insert(sky, cloudDiv)
+    }
+
+  }
 }
 
 
 const scatterStars = (sky, n) => {
-    for (let i=0; i<n; i++) {
-        const star = make("div", "class=star")
-        star.style.left = `${rand(5, 90)}%`
-        star.style.top = `${rand(5, 50)}%`
-        star.style.animationDelay = `${i}s`
-        insertAtStart(sky, star)
-    }
+  for (let i=0; i<n; i++) {
+    const star = make("div", "class=star")
+    star.style.left = `${rand(5, 90)}%`
+    star.style.top = `${rand(5, 50)}%`
+    star.style.animationDelay = `${i}s`
+    insertAtStart(sky, star)
+  }
 }
 
 
-const action = (cloudDiv) => {
+const action = async (cloudDiv) => {
+  
+  if (cloudDiv.id === `${NEXT_NUMBER}`) {
+
     cloudDiv.remove()
     // playRemoveSound
-    if (getClass("cloud").length === 0) {
-        // play completion sound
+    NEXT_NUMBER++
+
+    if (NEXT_NUMBER === 10) {
+      NEXT_NUMBER = 0
+      await sleep(100)
+      get("main").onclick = start
+      // play completion sound
     }
+
+  }
+
+  else {
+    // playWrongSound
+  }
+  
 }
 
 
 // ON PAGE LOAD
 // ================================================================================
 
+const start = () => {
+  const
+    main = get("main"),
+    sky = get("sky"),
+    peak = make("div", "class=peak"),
+    clouds = createClouds(10)
+  
+  main.onclick = null
+  clear(sky)
+  insert(sky, peak)
+  scatterClouds(sky, clouds)
+  scatterStars(sky, 10)
+}
+
+
 const main = () => {
+  const
+    main = get("main"),
+    sky = get("sky"),
+    peak = make("div", "class=peak")
 
-    let
-        sky = get("sky"),
-        peak = make("div", "class=peak")
-        clouds = createClouds(30)
-    
-    insert(sky, peak)
-    scatterStars(sky, 10)
-    scatterClouds(sky, clouds)
-
+  main.onclick = start
+  insert(sky, peak)
+  scatterStars(sky, 10)
 }
 
 
